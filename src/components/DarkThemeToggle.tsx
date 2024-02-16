9123483ede99f92956234dac11a450fa5df63b8e
Nikita Okhotnikov <nikiquark@gmail.com>
2024-02-16 17:37:59 +0700

    fix: next-themes for ssg

--- src/components/DarkThemeToggle.tsx
+++ src/components/DarkThemeToggle.tsx
@@ -2,9 +2,21 @@
 import { useTheme } from "next-themes";
 import { MdOutlineWbSunny } from "react-icons/md";
 import { FaRegMoon } from "react-icons/fa";
-
+import { useEffect, useState } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
 const DarkThemeToggle = () => {
+    const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme()
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) return (
+        <div className="pr-2 lg:pr-0 cursor-pointer text-yellow-200 hover:drop-shadow-[0_0_4px_rgba(254,240,138,1)] duration-300">
+            <AiOutlineLoading className="animate-spin drop-shadow-lg"  size={30}/>
+        </div>
+    );
+    
     return (
         <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className="pr-2 lg:pr-0 cursor-pointer text-yellow-200 hover:drop-shadow-[0_0_4px_rgba(254,240,138,1)] duration-300">
             {theme === "dark" ? <MdOutlineWbSunny className="drop-shadow-lg"  size={30}/> : <FaRegMoon className="" size={30}/>}
--- src/components/HoverInfo/HoverInfo.tsx
+++ src/components/HoverInfo/HoverInfo.tsx
@@ -6,7 +6,7 @@ const HoverInfo = ({children}:{children:React.ReactNode}) => {
         <div className="group lg:relative ">
             <IoInformationCircleOutline  className="text-xl lg:text-3xl cursor-pointer ml-2"/>
             <div className="hidden absolute left-0 lg:bottom-8 lg:-left-52 group-hover:block bg-gray-400 p-5 w-full overflow-x-scroll  lg:w-auto lg:overflow-hidden">
-                <p className="text-xs text-left">{children}</p>
+                <span className="text-xs text-left">{children}</span>
             </div>
             
         </div>
